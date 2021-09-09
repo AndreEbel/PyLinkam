764c7623b16f47da4ab147e21790fa7c18215249
AndreEbel <ebel@lct.u-bordeaux.fr>
2021-09-09 13:16:17 +0200

    bugfix

--- PyLinkam/__init__.py
+++ PyLinkam/__init__.py
@@ -5,6 +5,6 @@ Created on Wed Mar 24 11:04:31 2021
 @author: ebel
 """
 
-from .PyLinkam import programmer, datalog
+from .PyLinkam import programmer
 from .Pyqt_App import ControllerDisplay
 from .Pyqt_Widget import  ControllerThread, ControllerSimple
